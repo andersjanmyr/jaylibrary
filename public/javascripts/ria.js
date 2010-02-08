75f607598acc5d4cceb4dbd47cc37e918c35f394
Anders Janmyr <anders.janmyr@jayway.com>
2010-02-08 09:27:58 +0100

    first version of search working

--- public/javascripts/ria.js
+++ public/javascripts/ria.js
@@ -1,8 +1,23 @@
+jQuery.ajaxSetup({
+    'beforeSend': function(xhr) {
+        xhr.setRequestHeader('Accept', 'application/json');
+    }
+});
+
+function createBookItem(book) {
+    return '<li>' + book.title + '</li>';
+}
+
 jQuery(document).ready(function() {
     jQuery('#search-form').submit(function() {
         jQuery.get(jQuery(this).attr('action'), jQuery(this).serialize(), function(data) {
+            var results = jQuery('#search-results')
+            data.each(function(item) {
+                results.append(createBookItem(item.book));
+            })
         });
         return false;
     })
 
 });
+
